9010d6040c78033bdea6f941d5c73882dd577c7f
tgoldfrad <tgoldfrad8822@gmail.com>
2026-02-23 19:04:00 +0200

    changes2

--- backend/FinancialMonitor.Api/obj/Debug/net9.0/FinancialMonitor.Api.AssemblyInfo.cs
+++ backend/FinancialMonitor.Api/obj/Debug/net9.0/FinancialMonitor.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialMonitor.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c644411a9fc325a7fc9f152ab6aeec35772fa5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82a626b096feeccf8153490b5bb6969f74ccd3cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialMonitor.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialMonitor.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
